61b457bc08096e46ec2d22eda4200033f6651dd5
Navin Kumar <navin1551@gmail.com>
2019-10-25 20:22:11 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import shimano from "./assets/shimano.jpg";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [paidFor, setPaidFor] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  let paypalRef = useRef();
+
+  const product = {
+    price: 89.99,
+    description: "graphite Shimano fishing rod",
+    img: "assets/shimano.jpg"
+  };
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://www.paypal.com/sdk/js?client-id=AfDCVWJTflAAMVnxvFB4IN-vmEL1BaNRe5VRQjmyqcvj0OXP_cXOLsUBAvWX1teTDzRacGJmdExgILLp";
+    script.addEventListener("load", () => setLoaded(true));
+    document.body.appendChild(script);
+
+    if (loaded) {
+      setTimeout(() => {
+        window.paypal
+          .Buttons({
+            createOrder: (data, actions) => {
+              return actions.order.create({
+                purchase_units: [
+                  {
+                    description: product.description,
+                    amount: {
+                      currency_code: "USD",
+                      value: product.price
+                    }
+                  }
+                ]
+              });
+            },
+            onApprove: async (data, actions) => {
+              const order = await actions.order.capture();
+              setPaidFor(true);
+              console.log(order);
+            }
+          })
+          .render(paypalRef);
+      });
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        <h1>Navin's Store</h1>
+      </div>
+      <div>
+        {paidFor ? (
+          <div>
+            <h1 id="store-title">You just bought the fishing rod!</h1>
+          </div>
+        ) : (
+          <div>
+            <h2>
+              {product.description} for ${product.price}
+            </h2>
+            <img src={shimano} width="200" />
+            <div ref={v => (paypalRef = v)} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
